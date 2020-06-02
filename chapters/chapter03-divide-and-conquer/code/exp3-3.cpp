a8b3e6cffd861b9a7e0bbecd9208dccf706eb212
anlzou <599502931@qq.com>
2020-06-02 15:59:06 +0800

    feat(*03):update test8-3, add #2

--- chapters/chapter03-divide-and-conquer/code/exp3-3.cpp
+++ chapters/chapter03-divide-and-conquer/code/exp3-3.cpp
@@ -14,6 +14,9 @@
 
 using namespace std;
 
+/**
+ * 查找最大和次大元素
+ */
 void funFindMaxTop2(int arr[], int low, int high, int &max1, int &max2) {
     if (low == high) {  //区间只有一个元素
         max1 = arr[low];
@@ -38,12 +41,47 @@ void funFindMaxTop2(int arr[], int low, int high, int &max1, int &max2) {
     }
 }
 
+/**
+ * 折半查找/二分查找
+ */
+int funBinSearch(int arr[], int low, int high, int value) {
+    int mid;
+    if (low <= high) {            //当前区间存在元素时
+        mid = (low + high) / 2;   //求查找区间的中间位置
+        if (arr[mid] == value) {  //找到后返回其物理下标 mid
+            return mid;
+        }
+        if (arr[mid] >
+            value) {  //当 arr[mid]>value 时在 arr[low...mid-1] 中递归查找
+            return funBinSearch(arr, low, mid - 1, value);
+        } else {  //当 arr[mid]<value 时在 arr[mid+1...high] 中递归查找
+            return funBinSearch(arr, mid + 1, high, value);
+        }
+    } else {
+        return -1;  //当前查找区间没有元素时返回 -1
+    }
+}
+
+/*============================test==============================*/
+
 int main() {
-    // cout << INT32_MAX << endl;
+    // cout << INT32_MAX << endl;   //INF = 2147483647+1
     int arr[] = {5, 2, 1, 4, 3};
     int len = sizeof(arr) / sizeof(int);
-    int max1, max2;
-    funFindMaxTop2(arr, 0, len - 1, max1, max2);
-    cout << "max1:" << max1 << " max2:" << max2 << endl;
+
+    /*===============test funFindMaxTop2()================*/
+    // int max1, max2;
+    // funFindMaxTop2(arr, 0, len - 1, max1, max2);
+    // cout << "max1:" << max1 << " max2:" << max2 << endl;
+
+    /*================test funBinSearch()==================*/
+    int value = 4;
+    int index = funBinSearch(arr, 0, len - 1, value);
+    if (index >= 0) {
+        cout << "a[" << index << "]=" << value << endl;
+    } else {
+        cout << "not find the number:" << value << endl;
+    }
+
     return 0;
 }
--- chapters/chapter03-divide-and-conquer/test8-3.md
+++ chapters/chapter03-divide-and-conquer/test8-3.md
@@ -2,7 +2,7 @@
  * @Date        : 2020-05-02 20:37:47
  * @LastEditors : anlzou
  * @Github      : https://github.com/anlzou
- * @LastEditTime: 2020-06-02 15:18:41
+ * @LastEditTime: 2020-06-02 15:58:17
  * @FilePath    : \algorithm-design\chapters\chapter03-divide-and-conquer\test8-3.md
  * @Describe    : 
  -->
@@ -41,9 +41,20 @@
 3. 若 a[low...high] 中有两个以上元素，按中间位置 min = (low+high)/2 划分为 a[low...mid] 和 a[mid+1...high] 两个区间（注意左区间包含 a[mid]元素）。求出左区间的最大元素 lmax1 和次大元素 lmax2，求出右区间的最大元素 rmax1 和次大元素 rmax2。
 4. 若 lmax1 > rmax1，则 max1 = lmax1，max2 = max{lmax2, rmax1}；否则 max1 = rmax1，max2 = max{lmax1, rmax2}。
 
+#2
+
+**折半查找**又称二分查找，它是一种效率较高的查找方d法。但是折半查找要求查找列中的元素是有序的，为了简单，假设是递增有序的（前提是进行排序）。
+
+折半查找的基本思路：设 a[low...high] 是当前的查找区间，首先确定该区间的中点位置 mid = floor(low+high/2)；然后将待查的 k 值与 a[mid].key 比较。
+1. 若 k == a[mid].key，则查找成功并返回该元素的物理下标。
+2. 若 k < a[mid]，则由表的序列性可知 a[mid...high] 均大于 k，因此若表中存在关键字等于 K 的元素，则该元素必定位于左子表 a[low...mid-1] 中，故新的查找区间是左子表 a[low...mid-1]。
+3. 若 k > a[mid]，则要查找的 k 必定位于右子表 a[mid+1...high] 中，即新的查找区间是右子表 a[mid+1...high]。
+4. 下一次查找是针对新的查找区间进行的。因此可以从初始的查找区间 a[0...n-1] 开始，每经过一次与当前查找区间的中点位置上的关键字比较就确定查找是否成功，不成功则当前的查找区间缩小一半。重复这一过程，直到找到关键字为 k 的元素，或者直到当前的查找区间为空（即查找失败）时为止。 
+
 
 ## 代码
-#1
+#1、2
+
 [exp3-3.cpp](./code/exp3-3.cpp)
 
 ## 复杂度分析
