bc8067ef8a9a6cb107846f866928cf11c7007456
anlzou <599502931@qq.com>
2020-06-02 16:33:27 +0800

    feat(*03):update test8-3.cpp

--- chapters/chapter03-divide-and-conquer/code/exp3-3.cpp
+++ chapters/chapter03-divide-and-conquer/code/exp3-3.cpp
@@ -43,8 +43,9 @@ void funFindMaxTop2(int arr[], int low, int high, int &max1, int &max2) {
 
 /**
  * 折半查找/二分查找
+ * 递归
  */
-int funBinSearch(int arr[], int low, int high, int value) {
+int funRecuBinSearch(int arr[], int low, int high, int value) {
     int mid;
     if (low <= high) {            //当前区间存在元素时
         mid = (low + high) / 2;   //求查找区间的中间位置
@@ -53,20 +54,40 @@ int funBinSearch(int arr[], int low, int high, int value) {
         }
         if (arr[mid] >
             value) {  //当 arr[mid]>value 时在 arr[low...mid-1] 中递归查找
-            return funBinSearch(arr, low, mid - 1, value);
+            return funRecuBinSearch(arr, low, mid - 1, value);
         } else {  //当 arr[mid]<value 时在 arr[mid+1...high] 中递归查找
-            return funBinSearch(arr, mid + 1, high, value);
+            return funRecuBinSearch(arr, mid + 1, high, value);
         }
     } else {
         return -1;  //当前查找区间没有元素时返回 -1
     }
 }
 
+/**
+ * 折半查找/二分查找
+ * 非递归
+ */
+int funUnreBinSearch(int arr[], int len, int value) {
+    int low = 0, high = len - 1, mid;
+    while (low <= high) {         //当前区间存在元素时循环
+        mid = (low + high) / 2;   //求查找区间的中间位置
+        if (arr[mid] == value) {  //找到后返回其物理下标 mid
+            return mid;
+        }
+        if (arr[mid] > value) {  //继续在 arr[low...mid-1] 中继续查找
+            high = mid - 1;
+        } else {            // a[mid] < k
+            low = mid + 1;  //继续在 a[mid+1...high] 中查找
+        }
+    }
+    return -1;  //当前查找区间没有元素时返回 -1
+}
+
 /*============================test==============================*/
 
 int main() {
     // cout << INT32_MAX << endl;   //INF = 2147483647+1
-    int arr[] = {5, 2, 1, 4, 3};
+    int arr[] = {0, 1, 2, 3, 4, 5};
     int len = sizeof(arr) / sizeof(int);
 
     /*===============test funFindMaxTop2()================*/
@@ -75,10 +96,18 @@ int main() {
     // cout << "max1:" << max1 << " max2:" << max2 << endl;
 
     /*================test funBinSearch()==================*/
-    int value = 4;
-    int index = funBinSearch(arr, 0, len - 1, value);
+    int value = 2;
+    int index = funRecuBinSearch(arr, 0, len - 1, value);
     if (index >= 0) {
-        cout << "a[" << index << "]=" << value << endl;
+        cout << "arr[" << index << "]=" << value << endl;
+    } else {
+        cout << "not find the number:" << value << endl;
+    }
+
+    value = 1;
+    index = funUnreBinSearch(arr, len, value);
+    if (index >= 0) {
+        cout << "arr[" << index << "]=" << value << endl;
     } else {
         cout << "not find the number:" << value << endl;
     }
