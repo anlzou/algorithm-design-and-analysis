dc8740e5babc27960223e8d357636ef538d2624c
anlzou <599502931@qq.com>
2020-06-02 17:56:18 +0800

    feat(*03):update test8-3.md add #3

--- chapters/chapter03-divide-and-conquer/code/exp3-3.cpp
+++ chapters/chapter03-divide-and-conquer/code/exp3-3.cpp
@@ -41,6 +41,7 @@ void funFindMaxTop2(int arr[], int low, int high, int &max1, int &max2) {
     }
 }
 
+/*==============================折半查找===============================*/
 /**
  * 折半查找/二分查找
  * 递归
@@ -83,11 +84,44 @@ int funUnreBinSearch(int arr[], int len, int value) {
     return -1;  //当前查找区间没有元素时返回 -1
 }
 
-/*============================test==============================*/
+/*========================寻找一个序列中第 k 小的元素=========================*/
+/**
+ * 在 a[s...t] 序列中找第 k 小的元素
+ */
+int funQuickSelect(int arr[], int front, int rear, int k) {
+    int i = front, j = rear;
+    int temp;
+    if (front < rear) {     //区间内至少存在两个元素的情况
+        temp = arr[front];  //用区间的第 1 个记录作为基准
+        while (i != j) {  //从区间两端交替向中间扫描，直到 i=j 为止
+            while (j > i && arr[j] >= temp) {
+                j--;  //从右向左扫描，找第 1 个关键字小于 i=j 为止
+            }
+            arr[i] = arr[j];  //将 arr[j] 前移到 arr[i]的位置
+            while (i < j && arr[i] <= temp) {
+                i++;  //从左向右扫描，找第 1 个关键字大于 temp 的 arr[i]
+            }
+            arr[j] = arr[i];  //将 arr[i] 后移到 arr[j] 的位置
+        }
+        arr[i] = temp;
+        if (k - 1 == i) {
+            return arr[i];
+        } else if (k - 1 < i) {
+            return funQuickSelect(arr, front, i - 1, k);  //在左区间中递归查找
+        } else {
+            return funQuickSelect(arr, i + 1, rear, k);  //在右区间中递归查找
+        }
+    } else if (front == rear &&
+               front == k - 1) {  //区间内只有一个元素且为 arr[k-1]
+        return arr[k - 1];
+    }
+    return -1;
+}
 
+/*============================test==============================*/
 int main() {
     // cout << INT32_MAX << endl;   //INF = 2147483647+1
-    int arr[] = {0, 1, 2, 3, 4, 5};
+    int arr[] = {6, 1, 7, 3, 4, 5};
     int len = sizeof(arr) / sizeof(int);
 
     /*===============test funFindMaxTop2()================*/
@@ -96,20 +130,31 @@ int main() {
     // cout << "max1:" << max1 << " max2:" << max2 << endl;
 
     /*================test funBinSearch()==================*/
-    int value = 2;
-    int index = funRecuBinSearch(arr, 0, len - 1, value);
-    if (index >= 0) {
-        cout << "arr[" << index << "]=" << value << endl;
-    } else {
-        cout << "not find the number:" << value << endl;
-    }
+    // int value = 2;
+    // int index = funRecuBinSearch(arr, 0, len - 1, value);
+    // if (index >= 0) {
+    //     cout << "arr[" << index << "]=" << value << endl;
+    // } else {
+    //     cout << "not find the number:" << value << endl;
+    // }
 
-    value = 1;
-    index = funUnreBinSearch(arr, len, value);
-    if (index >= 0) {
-        cout << "arr[" << index << "]=" << value << endl;
-    } else {
-        cout << "not find the number:" << value << endl;
+    // value = 1;
+    // index = funUnreBinSearch(arr, len, value);
+    // if (index >= 0) {
+    //     cout << "arr[" << index << "]=" << value << endl;
+    // } else {
+    //     cout << "not find the number:" << value << endl;
+    // }
+
+    /*================test funQuickSelect()================*/
+    int index;
+    for (int k = 1; k <= len; k++) {
+        index = funQuickSelect(arr, 0, len - 1, k);
+        if (index != -1) {
+            cout << "第" << k << "小的元素为:" << index << endl;
+        } else {
+            cout << "error!" << endl;
+        }
     }
 
     return 0;
